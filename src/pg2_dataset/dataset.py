2677f45182d53822995353217d5f0b32dd84188b
Shushi <zhaobenben007@googlemail.com>
2025-05-06 17:20:21 +0200

    Update dataset.py

--- src/pg2_dataset/dataset.py
+++ src/pg2_dataset/dataset.py
@@ -10,7 +10,7 @@ class Dataset:
         targets: list[str] = [],
     ):
         if bool(set(features) & set(targets)):
-            raise ValueError(f"{features} and {targets} should share the same columns")
+            raise ValueError(f"{features} and {targets} should not share the same columns")
 
         self.features = list(set(features))
         self.targets = list(set(targets))
