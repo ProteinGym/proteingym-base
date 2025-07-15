fb9528deb55a18df48145f84db59fdecefa64854
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-07-15 15:15:08 +0200

    Move example TOML fixture to top-level

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -11,29 +11,31 @@ from pg2_dataset.dataset import Dataset, Manifest
 from pg2_dataset.primitives.meta import StructuresMeta
 
 
+@pytest.fixture
+def example_toml() -> str:
+    return """
+name = "test_name"
+description = "test_description"
+doi = "test_doi"
+source = "test_source"
+
+[structures_meta]
+file_path = "tests/test_data/structures/5kua_pdb.pdb"
+
+[assays_meta.assays.target1]
+features = ["feature1", "feature2"]
+description = "lorem ipsum"
+[assays_meta.assays.target1.constants]
+key_one = "1"
+key_two = 2
+
+[assays_meta.assays.target2]
+features = ["feature1"]
+description = "dolor sit amet"
+"""
+
+
 class TestDataset:
-    @pytest.fixture
-    def example_toml(self):
-        return """
-    name = "test_name"
-    description = "test_description"
-    doi = "test_doi"
-    source = "test_source"
-
-    [structures_meta]
-    file_path = "tests/test_data/structures/5kua_pdb.pdb"
-
-    [assays_meta.assays.target1]
-    features = ["feature1", "feature2"]
-    description = "lorem ipsum"
-    [assays_meta.assays.target1.constants]
-    key_one = "1"
-    key_two = 2
-
-    [assays_meta.assays.target2]
-    features = ["feature1"]
-    description = "dolor sit amet"
-    """
 
     def test_dataset_from_toml(self, example_toml):
         ds = Manifest.from_path(io.StringIO(example_toml)).ingest()
