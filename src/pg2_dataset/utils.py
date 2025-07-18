1bcafc603d4aff2985d45e0dcf17a3793fda540c
Prashant Srivastava <prashant.srivastava@xebia.com>
2025-07-18 11:28:16 +0200

    lint test_dataset and utils

--- src/pg2_dataset/utils.py
+++ src/pg2_dataset/utils.py
@@ -1,14 +1,16 @@
 import shutil
-from contextlib import contextmanager
 import zipfile
+from contextlib import contextmanager
 from pathlib import Path
-from typing import List, Iterator
+from typing import Iterator, List
+
 
 @contextmanager
 def zip_context(zip_path: str | Path) -> Iterator[List[Path]]:
     """Extract the contents of a ZIP within a context manager.
-    This will extract the ZIP contents to the current working directory and yield the list of extracted files.
-    After the context is exited, it will automatically clean up by removing the extracted files and directories.
+    This will extract the ZIP contents to the current working directory and
+    yield the list of extracted files. After the context is exited, it will
+    automatically clean up by removing the extracted files and directories.
     Example usage:
         with zip_context("path/to/archive.zip") as files:
             for file in files:
--- tests/models/test_dataset.py
+++ tests/models/test_dataset.py
@@ -1,24 +1,30 @@
-import pytest
-from pg2_dataset.models.sequence import Sequence
-from pg2_dataset.models.manifest import DatasetManifest
-from pg2_dataset.models.dataset import Dataset
 from pathlib import Path
+
+import pytest
+
+from pg2_dataset.models.dataset import Dataset
+from pg2_dataset.models.manifest import DatasetManifest
+from pg2_dataset.models.sequence import Sequence
+
 
 def test_assert_non_empty_sequence_list_raises():
     from pg2_dataset.models.dataset import assert_non_empty_sequence_list
+
     with pytest.raises(ValueError):
         assert_non_empty_sequence_list([])
 
 
 @pytest.mark.parametrize(
-    "name, description, version, sequences", 
+    "name, description, version, sequences",
     [
         ("test_dataset", "A test dataset which contains sequences.", "1.0", []),
-    ]
+    ],
 )
 @pytest.mark.xfail(raises=ValueError, reason="At least one sequence is required.")
 def test_dataset_empty_sequences(name, description, version, sequences):
-    ds = Dataset(name=name, description=description, version=version, sequences=sequences)
+    ds = Dataset(
+        name=name, description=description, version=version, sequences=sequences
+    )
     assert ds.name == name
     assert ds.description == description
     assert ds.version == version
@@ -26,19 +32,28 @@ def test_dataset_empty_sequences(name, description, version, sequences):
 
 
 @pytest.mark.parametrize(
-    "name, description, version, sequences", 
+    "name, description, version, sequences",
     [
-        ("test_dataset", "A test dataset which contains sequences.", "1.0", [{
+        (
+            "test_dataset",
+            "A test dataset which contains sequences.",
+            "1.0",
+            [
+                {
                     "name": "seq1",
                     "value": "ACGT",
                     "description": "seq 1 desc",
                     "type": "wild_type",
                     "alphabet": "DNA",
-                }]),
-    ]
+                }
+            ],
+        ),
+    ],
 )
 def test_dataset(name, description, version, sequences):
-    ds = Dataset(name=name, description=description, version=version, sequences=sequences)
+    ds = Dataset(
+        name=name, description=description, version=version, sequences=sequences
+    )
     assert ds.name == name
     assert ds.description == description
     assert ds.version == version
@@ -59,12 +74,12 @@ def test_dataset(name, description, version, sequences):
                     "sequence_alphabet": "DNA",
                     "sources": {
                         "local": ["tests/test_data/datasets/dataset1/sequences/"],
-                        "s3": []
-                    }
+                        "s3": [],
+                    },
                 }
             ],
         }
-    ]
+    ],
 )
 def test_dataset_from_manifest(manifest):
     print(manifest)
@@ -85,7 +100,7 @@ def test_dataset_from_manifest(manifest):
     "manifest_path",
     [
         "tests/test_data/datasets/dataset1/neime_manifest.toml",
-    ]
+    ],
 )
 def test_dataset_from_manifest_toml(manifest_path):
     ds = Dataset.from_manifest_toml(manifest_path)
@@ -101,7 +116,7 @@ def test_dataset_from_manifest_toml(manifest_path):
     "zip_path",
     [
         "tests/test_data/datasets/zip/neime.zip",
-    ]
+    ],
 )
 def test_dataset_from_zip(zip_path):
     ds = Dataset.from_zip(zip_path)
@@ -112,20 +127,30 @@ def test_dataset_from_zip(zip_path):
     assert len(ds.sequences) > 0
     assert all(isinstance(seq, Sequence) for seq in ds.sequences)
 
+
 @pytest.mark.parametrize(
-    "name, description, version, sequences", 
+    "name, description, version, sequences",
     [
-        ("test_dataset", "A test dataset which contains sequences.", "1.0", [{
+        (
+            "test_dataset",
+            "A test dataset which contains sequences.",
+            "1.0",
+            [
+                {
                     "name": "seq1",
                     "value": "ACGT",
                     "description": "seq 1 desc",
                     "type": "wild_type",
                     "alphabet": "DNA",
-                }]),
-    ]
+                }
+            ],
+        ),
+    ],
 )
 def test_dataset_dump(name, description, version, sequences):
-    ds = Dataset(name=name, description=description, version=version, sequences=sequences)
+    ds = Dataset(
+        name=name, description=description, version=version, sequences=sequences
+    )
     ds.dump("test_dir/")
     assert Path(f"test_dir/{name}.toml").exists()
-    assert Path(f"test_dir/sequences/seq1.fasta").exists()
+    assert Path("test_dir/sequences/seq1.fasta").exists()
