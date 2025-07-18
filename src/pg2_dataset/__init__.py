204ec4835fa05c79dc98c9be8ced6f0234b47041
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-07-18 12:25:27 +0200

    Add Dataset and Manifest as entrypoint to the package

---
+++ src/pg2_dataset/__init__.py
@@ -0,0 +1,19 @@
+"""The protein gym 2 dataset.
+
+A standardized format for storing and sharing datasets in the Protein Gym 2
+ecosystem.
+
+Attributes :
+    Dataset :
+        The main class for managing datasets.
+    Manifest :
+        Represents the metadata and resources of a dataset.  Entrypoint for
+        loading and validating dataset from metadata.
+"""
+
+from .models.dataset import Dataset, Manifest
+
+__all__ = [
+    "Dataset",
+    "Manifest",
+]
