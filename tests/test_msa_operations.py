9d9531e6814687cb90c1f9301abd873664302f64
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-09-11 13:13:15 +0200

    Test msa operations

---
+++ tests/test_msa_operations.py
@@ -0,0 +1,41 @@
+"""
+Module for testing MSA operators.
+"""
+
+from Bio.Align import MultipleSeqAlignment
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+from pg2_dataset.msa import MSA
+
+
+def test_msa_equals_itself():
+    """An MSA should equal itself."""
+    alignment = MultipleSeqAlignment(
+        [
+            SeqRecord(Seq("ACDEFG"), id="seq1"),
+            SeqRecord(Seq("GFEDCA"), id="seq2"),
+        ]
+    )
+    msa = MSA(
+        name="Test MSA",
+        value=alignment,
+        description=None,
+    )
+    assert msa == msa
+
+
+def test_msa_with_data_equals_itself():
+    """An MSA with data should equal itself."""
+    alignment = MultipleSeqAlignment(
+        [
+            SeqRecord(Seq("ACDEFG"), id="seq1"),
+            SeqRecord(Seq("GFEDCA"), id="seq2"),
+        ]
+    )
+    msa = MSA(
+        name="Test MSA",
+        value=alignment,
+        description="A test MSA",
+    )
+    assert msa == msa
