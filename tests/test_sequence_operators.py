d99906d4d35493446069503a1996cb451d432d0d
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-09-18 16:33:17 +0200

    Run pre-commit

--- tests/test_sequence_operators.py
+++ tests/test_sequence_operators.py
@@ -2,17 +2,28 @@
 Module for testing sequence operators.
 """
 
-from pg2_dataset.sequence import Sequence, SequenceType, SequenceAlphabet
 from Bio.Seq import Seq
+
+from pg2_dataset.sequence import Sequence, SequenceAlphabet, SequenceType
 
 
 def test_sequence_equals_itself():
     """A sequence should equal itself."""
-    sequence = Sequence(name="Test Sequence", value=Seq("") , type=SequenceType.WILD_TYPE, alphabet=SequenceAlphabet.AA)
+    sequence = Sequence(
+        name="Test Sequence",
+        value=Seq(""),
+        type=SequenceType.WILD_TYPE,
+        alphabet=SequenceAlphabet.AA,
+    )
     assert sequence == sequence
 
 
 def test_sequence_with_data_equals_itself():
     """A sequence with data should equal itself."""
-    sequence = Sequence(name="Test Sequence", value=Seq("ACDEFG"), type=SequenceType.WILD_TYPE, alphabet=SequenceAlphabet.AA)
+    sequence = Sequence(
+        name="Test Sequence",
+        value=Seq("ACDEFG"),
+        type=SequenceType.WILD_TYPE,
+        alphabet=SequenceAlphabet.AA,
+    )
     assert sequence == sequence
