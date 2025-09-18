3a3104a7dc6b94e90e3914b32aad631f6120a670
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-09-18 14:42:20 +0200

    Add type hints

--- tests/test_assays_operators.py
+++ tests/test_assays_operators.py
@@ -5,31 +5,31 @@ Module for testing assay operators.
 from pg2_dataset.assay import Assay
 
 
-def test_assay_not_equal_to_integer():
+def test_assay_not_equal_to_integer() -> None:
     """An assay is not equal to an integer"""
     assay = Assay(name="Test Assay", records=[])
     assert assay != 1
 
 
-def test_assay_empty_equals_itself():
+def test_assay_empty_equals_itself() -> None:
     """An empty assay should equal itself."""
     assay = Assay(name="Test Assay", records=[])
     assert assay == assay
 
 
-def test_assay_empty_contains_itself():
+def test_assay_empty_contains_itself() -> None:
     """An empty assay should contain itself (via set operations)."""
     assay = Assay(name="Test Assay", records=[])
     assert assay in assay
 
 
-def test_assay_with_record_equals_itself():
+def test_assay_with_record_equals_itself() -> None:
     """An assay with a record should equal itself."""
     assay = Assay(name="Test Assay", records=[("SEQ1", 1.0)])
     assert assay == assay
 
 
-def test_assay_with_record_contains_itself():
+def test_assay_with_record_contains_itself() -> None:
     """An assay with a record should contain itself."""
     assay = Assay(name="Test Assay", records=[("SEQ1", 1.0)])
     assert assay in assay
