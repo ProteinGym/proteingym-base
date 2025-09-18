2bcb3947eaacc415de4761f49c732d0259c905b1
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-09-18 14:42:03 +0200

    Add test about assay not equal to integer
    For coverage reasons

--- tests/test_assays_operators.py
+++ tests/test_assays_operators.py
@@ -5,6 +5,12 @@ Module for testing assay operators.
 from pg2_dataset.assay import Assay
 
 
+def test_assay_not_equal_to_integer():
+    """An assay is not equal to an integer"""
+    assay = Assay(name="Test Assay", records=[])
+    assert assay != 1
+
+
 def test_assay_empty_equals_itself():
     """An empty assay should equal itself."""
     assay = Assay(name="Test Assay", records=[])
@@ -27,4 +33,3 @@ def test_assay_with_record_contains_itself():
     """An assay with a record should contain itself."""
     assay = Assay(name="Test Assay", records=[("SEQ1", 1.0)])
     assert assay in assay
-
