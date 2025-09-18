d78e3f7cfcf562c590bdb1470df5b7f07fe3ac0c
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-09-18 16:33:41 +0200

    Test Sequence not equal to itneger

--- tests/test_sequence_operators.py
+++ tests/test_sequence_operators.py
@@ -7,6 +7,17 @@ from Bio.Seq import Seq
 from pg2_dataset.sequence import Sequence, SequenceAlphabet, SequenceType
 
 
+def test_sequence_not_equals_integer():
+    """A sequence should not equal an integer."""
+    sequence = Sequence(
+        name="Test Sequence",
+        value=Seq(""),
+        type=SequenceType.WILD_TYPE,
+        alphabet=SequenceAlphabet.AA,
+    )
+    assert sequence != 1
+
+
 def test_sequence_equals_itself():
     """A sequence should equal itself."""
     sequence = Sequence(
