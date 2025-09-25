f3c0ef2063fd2a70b59db7830b7c80711fbde413
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-09-25 10:11:07 +0200

    Fix reference to key
    Should be value instead

--- tests/test_assay_operators.py
+++ tests/test_assay_operators.py
@@ -104,8 +104,8 @@ def test_assay_contains_includes_condition_mismatch() -> None:
     assert subset not in assay
 
 
-def test_assay_contains_includes_condition_key_mismatch() -> None:
-    """Condition keys should be considered for equality."""
+def test_assay_contains_includes_condition_value_mismatch() -> None:
+    """Condition values should be considered for equality."""
     assay = Assay(
         name="Test assay",
         records=[("SEQ1", 1.0), ("SEQ2", 2.0)],
