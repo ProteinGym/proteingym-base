4c755bd5dd15c173f8eb52ab5f75ef7787f01a06
Shushi <zhaobenben007@googlemail.com>
2025-09-29 15:18:06 +0200

    Update __main__.py

--- src/pg2_dataset/__main__.py
+++ src/pg2_dataset/__main__.py
@@ -101,7 +101,7 @@ def build(
     typer.echo(f"Dataset {dataset.name} archived to: {archive_path}")
 
 
-@app.command("list_datasets")
+@app.command("list")
 def list_datasets(
     path: Annotated[
         Path,
