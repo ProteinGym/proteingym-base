6a2de94ff6df15beb390fe356432e4ca9b29ac5e
Prashant Srivastava <srivastava.prashant898@gmail.com>
2025-09-30 09:10:56 +0200

    change alphabet from str to SequenceAlphabet (#316)
    * change alphabet from str to SequenceAlphabet
    
    * add docstring

--- src/proteingym/base/assay.py
+++ src/proteingym/base/assay.py
@@ -127,7 +127,8 @@ class Assay:
     records: list[tuple[Sequence, int | float | bool | str]]
     """The records of the assay, pairs of Sequence and target values."""
 
-    sequence_alphabet: str
+    sequence_alphabet: SequenceAlphabet
+    """The alphabet of the sequences in the assay."""
 
     variables: dict[str, int | float | bool | str] = dataclasses.field(
         default_factory=dict
