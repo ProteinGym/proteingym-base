c9701eaeb81c8a4b40009d3486235cc5b9a2f0b9
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-10-16 08:59:59 +0200

    Add and test assay length

--- tests/test_assay_operators.py
+++ tests/test_assay_operators.py
@@ -8,6 +8,31 @@ from proteingym.base.assay import Assay
 from proteingym.base.sequence import Sequence, SequenceAlphabet, SequenceType
 
 
+@pytest.mark.parametrize(
+    "records",
+    [
+        [],
+        [
+            (
+                Sequence(
+                    name="seq1",
+                    value="ACD",
+                    type=SequenceType.WILD_TYPE,
+                    alphabet=SequenceAlphabet.AA,
+                ),
+                1.0,
+            )
+        ],
+    ],
+)
+def test_assay_length_equals_records_length(
+    records: list[tuple[Sequence | str | int | float | bool | str]],
+) -> None:
+    """The assay length should equal the number of records."""
+    assay = Assay(name="Test Assay", records=records)
+    assert len(assay) == len(records)
+
+
 def test_assay_not_equal_to_integer() -> None:
     """An assay is not equal to an integer"""
     assay = Assay(name="Test Assay", records=[])
