01dfaffb3e5daed72595c112fe6398985660227d
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-10-16 08:21:59 +0200

    Test random split length equals fractions length

--- tests/test_splits.py
+++ tests/test_splits.py
@@ -18,3 +18,11 @@ def test_random_splitter_raises_value_error_if_fraction_below_zero(
     """Test that RandomSplitter raises ValueError if fractions do not sum to 1."""
     with pytest.raises(ValueError, match="Fractions must be positive numbers."):
         RandomSplitter(dataset=empty_dataset, fractions=[0.7, 0.4, -0.1])
+
+
+def test_random_splitter_splits_length(empty_dataset: Dataset):
+    """Test that RandomSplitter splits the dataset into the correct number of slices."""
+    fractions = [0.8, 0.2]
+    splitter = RandomSplitter(dataset=empty_dataset, fractions=fractions)
+    superset = splitter.split()
+    assert len(superset) == len(fractions)
