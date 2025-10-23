d63d2409bc2ed8925400149aaf9b6e7cf80cad20
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-10-23 09:43:43 +0200

    Add test return type

--- tests/test_splits.py
+++ tests/test_splits.py
@@ -6,7 +6,7 @@ from proteingym.base.splits import RandomSplitter
 
 def test_random_splitter_raises_value_error_if_fractions_do_not_sum_to_one(
     empty_dataset: Dataset,
-):
+) -> None:
     """Test that RandomSplitter raises ValueError if fractions do not sum to 1."""
     with pytest.raises(ValueError, match="Fractions must sum to 1."):
         RandomSplitter(dataset=empty_dataset, fractions=[0.7, 0.3, 0.1])
@@ -14,13 +14,13 @@ def test_random_splitter_raises_value_error_if_fractions_do_not_sum_to_one(
 
 def test_random_splitter_raises_value_error_if_fraction_below_zero(
     empty_dataset: Dataset,
-):
+) -> None:
     """Test that RandomSplitter raises ValueError if fractions do not sum to 1."""
     with pytest.raises(ValueError, match="Fractions must be positive numbers."):
         RandomSplitter(dataset=empty_dataset, fractions=[0.7, 0.4, -0.1])
 
 
-def test_random_splitter_splits_length(empty_dataset: Dataset):
+def test_random_splitter_splits_length(empty_dataset: Dataset) -> None:
     """Test that RandomSplitter splits the dataset into the correct number of slices."""
     fractions = [0.8, 0.2]
     splitter = RandomSplitter(dataset=empty_dataset, fractions=fractions)
@@ -37,7 +37,7 @@ def test_random_splitter_splits_length(empty_dataset: Dataset):
 )
 def test_random_splitter_splits_in_dataset(
     dataset_with_assay: Dataset, fractions: list[float]
-):
+) -> None:
     """Test that RandomSplitter splits the dataset into the correct number of slices."""
     splitter = RandomSplitter(dataset=dataset_with_assay, fractions=fractions)
     superset = splitter.split()
