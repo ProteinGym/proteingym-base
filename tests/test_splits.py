928d826e2ee95150890b22841418494fe3b9531c
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-10-23 10:28:08 +0200

    Remove fixture reference

--- tests/test_splits.py
+++ tests/test_splits.py
@@ -57,7 +57,6 @@ def test_random_splitter_splits_in_dataset(
 @pytest.mark.parametrize(
     "dataset",
     [
-        "dataset_with_empty_assay",
         "dataset_with_single_assay",
         "dataset_with_multiple_assays",
     ],
