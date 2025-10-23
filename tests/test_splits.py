9c7c98cbd3c6c1af73c91dea03d4e104b7932000
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-10-23 17:06:44 +0200

    Test kfolds keeps all records

--- tests/test_splits.py
+++ tests/test_splits.py
@@ -1,3 +1,7 @@
+import functools
+
+import polars as pl
+import polars.testing
 import pytest
 
 from proteingym.base import Dataset
@@ -224,6 +228,7 @@ def test_random_splitter_splits_are_disjoint(dataset: Dataset) -> None:
     split_first, split_second = tuple(splitter.split(dataset=dataset))
     assert split_first not in split_second
     assert split_second not in split_first
+<<<<<<< HEAD
 from proteingym.base.splits import RandomSplitter
 
 
@@ -247,6 +252,8 @@ def test_random_splitter_splits_length(dataset_empty: Dataset) -> None:
     splitter = RandomSplitter(fractions)
     subsets = splitter.split(dataset_empty)
     assert len(subsets) == len(fractions)
+=======
+>>>>>>> 672ad39 (Test kfolds keeps all records)
 
 
 @pytest.mark.parametrize(
@@ -258,6 +265,7 @@ def test_random_splitter_splits_length(dataset_empty: Dataset) -> None:
     ],
     indirect=True,
 )
+<<<<<<< HEAD
 @pytest.mark.parametrize(
     "fractions",
     [
@@ -301,3 +309,20 @@ def test_kfold_splitter_splits_are_disjoint(dataset: Dataset) -> None:
                 continue
             assert split_first not in split_second
             assert split_second not in split_first
+=======
+@pytest.mark.parametrize("n_splits", [2, 3, 5])
+def test_kfold_splitter_splits_contain_all_records(
+    dataset: Dataset, n_splits: int
+) -> None:
+    """Test that KFoldSplitter splits contain all records from the original dataset."""
+    splitter = KFoldSplitter(dataset=dataset, n_splits=n_splits)
+    dataset_with_all_splits = functools.reduce(lambda d1, d2: d1 | d2, splitter.split())
+    # Using a dataframe comparision here as the dataset reconstructed from the
+    # folds will have the records spread over multiple assays
+    pl.testing.assert_frame_equal(
+        dataset.to_df(),
+        dataset_with_all_splits.to_df(),
+        check_dtypes=False,
+        check_column_order=False,
+    )
+>>>>>>> 672ad39 (Test kfolds keeps all records)
