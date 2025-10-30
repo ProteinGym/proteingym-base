e54ee3b8a01364e713af73582ba5a52ebee2ea3d
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-10-30 13:51:44 +0100

    Remove supserset

--- tests/test_splits.py
+++ tests/test_splits.py
@@ -132,8 +132,8 @@ def test_random_splitter_splits_length(dataset_empty: Dataset) -> None:
     """Test that RandomSplitter splits the dataset into the correct number of slices."""
     fractions = [0.8, 0.2]
     splitter = RandomSplitter(fractions)
-    superset = splitter.split(dataset_empty)
-    assert len(superset) == len(fractions)
+    subsets = splitter.split(dataset_empty)
+    assert len(subsets) == len(fractions)
 
 
 @pytest.mark.parametrize(
@@ -157,8 +157,74 @@ def test_random_splitter_splits_in_dataset(
 ) -> None:
     """Test that RandomSplitter splits the dataset into the correct number of slices."""
     splitter = RandomSplitter(fractions)
-    superset = splitter.split(dataset)
-    for i, split in enumerate(superset):
+    subsets = splitter.split(dataset)
+    for i, split in enumerate(subsets):
+        assert split in dataset, f"Split {i + 1} not in original dataset."
+
+
+@pytest.mark.parametrize(
+    "dataset",
+    [
+        "dataset_with_single_assay",
+        "dataset_with_multiple_assays",
+    ],
+    indirect=True,
+)
+def test_random_splitter_splits_are_disjoint(dataset: Dataset) -> None:
+    """Test that RandomSplitter splits are disjoint."""
+    fractions = [0.5, 0.5]
+    splitter = RandomSplitter(fractions)
+    split_first, split_second = tuple(splitter.split(dataset=dataset))
+    assert split_first not in split_second
+    assert split_second not in split_first
+from proteingym.base.splits import RandomSplitter
+
+
+def test_random_splitter_raises_value_error_if_fractions_do_not_sum_to_one(
+    dataset_empty: Dataset,
+) -> None:
+    """Test that RandomSplitter raises ValueError if fractions do not sum to 1."""
+    with pytest.raises(ValueError, match="Fractions must sum to 1."):
+        RandomSplitter(fractions=[0.7, 0.3, 0.1])
+
+
+def test_random_splitter_raises_value_error_if_fraction_below_zero() -> None:
+    """Test that RandomSplitter raises ValueError if fractions do not sum to 1."""
+    with pytest.raises(ValueError, match="Fractions must be positive numbers."):
+        RandomSplitter(fractions=[0.7, 0.4, -0.1])
+
+
+def test_random_splitter_splits_length(dataset_empty: Dataset) -> None:
+    """Test that RandomSplitter splits the dataset into the correct number of slices."""
+    fractions = [0.8, 0.2]
+    splitter = RandomSplitter(fractions)
+    subsets = splitter.split(dataset_empty)
+    assert len(subsets) == len(fractions)
+
+
+@pytest.mark.parametrize(
+    "dataset",
+    [
+        "dataset_with_empty_assay",
+        "dataset_with_single_assay",
+        "dataset_with_multiple_assays",
+    ],
+    indirect=True,
+)
+@pytest.mark.parametrize(
+    "fractions",
+    [
+        [0.5, 0.5],  # Each split with one record
+        [0.9, 0.1],  # One split with no records
+    ],
+)
+def test_random_splitter_splits_in_dataset(
+    dataset: Dataset, fractions: list[float]
+) -> None:
+    """Test that RandomSplitter splits the dataset into the correct number of slices."""
+    splitter = RandomSplitter(fractions)
+    subsets = splitter.split(dataset)
+    for i, split in enumerate(subsets):
         assert split in dataset, f"Split {i + 1} not in original dataset."
 
 
