65bbd1f973dd047ea3979d3cdaca0112e14b21ce
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-10-30 08:57:43 +0100

    Refactor to expect dataset in split instead of init

--- tests/test_splits.py
+++ tests/test_splits.py
@@ -119,22 +119,20 @@ def test_random_splitter_raises_value_error_if_fractions_do_not_sum_to_one(
 ) -> None:
     """Test that RandomSplitter raises ValueError if fractions do not sum to 1."""
     with pytest.raises(ValueError, match="Fractions must sum to 1."):
-        RandomSplitter(dataset=dataset_empty, fractions=[0.7, 0.3, 0.1])
+        RandomSplitter(fractions=[0.7, 0.3, 0.1])
 
 
-def test_random_splitter_raises_value_error_if_fraction_below_zero(
-    dataset_empty: Dataset,
-) -> None:
+def test_random_splitter_raises_value_error_if_fraction_below_zero() -> None:
     """Test that RandomSplitter raises ValueError if fractions do not sum to 1."""
     with pytest.raises(ValueError, match="Fractions must be positive numbers."):
-        RandomSplitter(dataset=dataset_empty, fractions=[0.7, 0.4, -0.1])
+        RandomSplitter(fractions=[0.7, 0.4, -0.1])
 
 
 def test_random_splitter_splits_length(dataset_empty: Dataset) -> None:
     """Test that RandomSplitter splits the dataset into the correct number of slices."""
     fractions = [0.8, 0.2]
-    splitter = RandomSplitter(dataset=dataset_empty, fractions=fractions)
-    superset = splitter.split()
+    splitter = RandomSplitter(fractions)
+    superset = splitter.split(dataset_empty)
     assert len(superset) == len(fractions)
 
 
@@ -158,8 +156,8 @@ def test_random_splitter_splits_in_dataset(
     dataset: Dataset, fractions: list[float]
 ) -> None:
     """Test that RandomSplitter splits the dataset into the correct number of slices."""
-    splitter = RandomSplitter(dataset=dataset, fractions=fractions)
-    superset = splitter.split()
+    splitter = RandomSplitter(fractions)
+    superset = splitter.split(dataset)
     for i, split in enumerate(superset):
         assert split in dataset, f"Split {i + 1} not in original dataset."
 
@@ -175,7 +173,7 @@ def test_random_splitter_splits_in_dataset(
 def test_random_splitter_splits_are_disjoint(dataset: Dataset) -> None:
     """Test that RandomSplitter splits are disjoint."""
     fractions = [0.5, 0.5]
-    splitter = RandomSplitter(dataset=dataset, fractions=fractions)
-    split_first, split_second = tuple(splitter.split())
+    splitter = RandomSplitter(fractions)
+    split_first, split_second = tuple(splitter.split(dataset=dataset))
     assert split_first not in split_second
     assert split_second not in split_first
