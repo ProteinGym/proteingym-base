70150dfaf39138edaf79e5c21f6c3335eaa303fd
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-12-04 11:07:11 +0100

    Test raising value error for unuspported polars type

--- tests/test_assay.py
+++ tests/test_assay.py
@@ -129,6 +129,19 @@ def test_field_polars_type(
     assert field.polars_type == polars_type
 
 
+def test_field_unsupported_polars_type_raises_value_error() -> None:
+    """An unsupported Field type should raise a ValueError."""
+    value = object()
+    try:
+        polars_type = Field(name="field", value=value).polars_type
+    except ValueError:
+        assert True
+    else:
+        raise AssertionError(
+            f"Value should not be mapped to a Polars type: {value} -> {polars_type}"
+        )
+
+
 def test_assay_variable_minimal() -> None:
     """Test creating a minimal AssayVariable."""
     # This should not raise an error
