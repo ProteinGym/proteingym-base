88e18a98ebd1064c1b193e419444ad694b29a23a
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-12-04 08:56:13 +0100

    Compare extension including period

--- src/proteingym/base/measurement.py
+++ src/proteingym/base/measurement.py
@@ -77,7 +77,7 @@ class MeasurementsManifestSection(BaseModel):
         """Optionally, extend the path with the `relative_to_path` from the context."""
         if info.context and info.context.get("relative_to_path"):
             path = info.context["relative_to_path"] / path
-        fmt = path.suffix[1:].lower()
+        fmt = path.suffix.lower()
         if fmt not in MeasurementsFormat:
             raise ValueError(f"Unsupported measurements file format: {fmt}")
         return path
