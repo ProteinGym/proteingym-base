5b4b3cb7e7a3722ea72cdc51bafc6bf2700298c8
Cor Zuurmond <cor.zuurmond@xebia.com>
2025-12-04 08:53:00 +0100

    Align docs with manifest in test

--- tests/test_manifest.py
+++ tests/test_manifest.py
@@ -41,6 +41,15 @@ sequence_alphabet = "AA"
 [ assays.variables ]
 PH = "7"
 
+[[ measurements ]]
+name = "assay"
+path = "measurements.csv"
+
+[[ measurements.fields ]]
+name = "OD"
+unit = "log10(l0 / l)"
+description = "Optical density measurement at 600nm"
+
 [[ sequences ]]
 type = "wild_type"
 alphabet = "DNA"
@@ -71,7 +80,15 @@ def manifest_path(tmp_path: Path, manifest_contents: str) -> Path:
     msa_file = tmp_path / "msas.a3m"
     msa_weights_file = tmp_path / "weights.npy"
     assay_file = tmp_path / "assay.csv"
-    for path in sequence_file, structure_file, msa_file, msa_weights_file, assay_file:
+    measurement_file = tmp_path / "measurements.csv"
+    for path in (
+        sequence_file,
+        structure_file,
+        msa_file,
+        msa_weights_file,
+        assay_file,
+        measurement_file,
+    ):
         path.touch()
     # Write header in the assay file
     assay_file.write_text("sequence,DMS_score,DMS_score_bin\n")
